22a0dbdb5f5ba91b8c4a9796f99039c25dd5dc0a
Vinicius Nascimento Rodrigues <viniciusrodrig2002@gmail.com>
2024-02-22 14:47:45 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto
+{
+    class lampada
+    {
+        public int potencia;
+        public string tipo;
+        public double preco;
+        public bool Aceso = false;
+
+
+        public bool ligar()
+        {
+            Aceso = true;
+            return Aceso;
+        }
+
+        public bool Desligar()
+        {
+            Aceso = false;
+            return Aceso;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            lampada l1 = new lampada();
+            l1.potencia = 100; l1.preco = 32.5;
+            l1.ligar();l1.Desligar();
+            if (l1.Aceso == true) 
+            {
+                Console.WriteLine("Aceso");
+            }
+            else
+            {
+                Console.WriteLine("Apagado");
+            }
+        }
+    }
+}
